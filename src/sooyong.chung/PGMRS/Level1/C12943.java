1376e7da988eb22943f4c99a27b2fb3ad9a0e8d1
aaplatform <41350882+aaplatform@users.noreply.github.com>
2018-09-04 14:45:04 +0900

    [Level1] 콜라츠 추측
    테스트케이스 중 일부가 Integer 타입을 오버하는 경우가 있어 long 타입으로 작업을 수행하도록 해야합니다.

---
+++ src/sooyong.chung/PGMRS/Level1/C12943.java
@@ -0,0 +1,24 @@
+class Solution {
+  public int solution(int n) {
+      int answer = 0;
+      
+      long num = n;
+      
+      while(num != 1) {          
+          if(answer > 500) {
+              answer = -1;
+              break;
+          }
+          if(num % 2 == 0) {
+              num /= 2;
+          } else {
+              num *= 3;
+              num++;
+          }
+          
+          answer++;
+      }
+      
+      return answer;
+  }
+}
